37fc9646316722fea6e8236e5e3b787bc2436e33
hyunphj <qkrwlgus4001@gmail.com>
2024-12-16 15:27:51 +0900

    default

---
+++ src/test/java/com/example/nbe232team2/Nbe232Team2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nbe232team2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nbe232Team2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
