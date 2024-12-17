9ad93e885cc5c67d8951b0e56ada2d49e7ad5c5a
suho <yousuho1232@naver.com>
2024-12-17 22:39:28 +0900

    feat : 홈페이지 구현(헤더 구현, 영화 상세 정보 구현, 상영 영화 구현, 개봉 예정 영화 구현) #scrum-23, #scrum-24, #scrum-25
    refactor : Movie entity 수정, Status 수정

---
+++ src/main/java/com/example/letmovie/domain/movie/service/MovieServiceImpl.java
@@ -0,0 +1,28 @@
+package com.example.letmovie.domain.movie.service;
+
+import com.example.letmovie.domain.movie.entity.Movie;
+import com.example.letmovie.domain.movie.repository.MovieJpaRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+@Transactional
+public class MovieServiceImpl {
+
+    private final MovieJpaRepository movieJpaRepository;
+
+    public MovieServiceImpl(MovieJpaRepository movieJpaRepository) {
+        this.movieJpaRepository = movieJpaRepository;
+    }
+
+    public List<Movie> getAllMovies() {
+        return movieJpaRepository.findAll();
+    }
+
+    public Movie getMovieById(int movieId) {
+        return movieJpaRepository.findById(movieId).orElse(null);
+    }
+}
