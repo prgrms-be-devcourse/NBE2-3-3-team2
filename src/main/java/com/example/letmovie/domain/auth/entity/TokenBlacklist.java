f07f3d8fbb0ebefa6bc97988fd52db15ea119ebb
hjjo <hjjo0927@gmail.com>
2024-12-19 16:57:44 +0900

    feat : JWT 토큰 사용을 위한 작업 준비
    build.gradle에 jwt, security 추가
    application.property 내 jwt 설정 추가
    SecurityConfig, 인증 작업처리를 위한 TokenBlacklist 엔티티 추가

---
+++ src/main/java/com/example/letmovie/domain/auth/entity/TokenBlacklist.java
@@ -0,0 +1,29 @@
+package com.example.letmovie.domain.auth.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class TokenBlacklist {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "token_id")
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String refreshToken;
+
+    @Column(nullable = false)
+    private LocalDateTime expiryDate;
+
+    public TokenBlacklist(String refreshToken, LocalDateTime expiryDate) {
+        this.refreshToken = refreshToken;
+        this.expiryDate = expiryDate;
+    }
+}
