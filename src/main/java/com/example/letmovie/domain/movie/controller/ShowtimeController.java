14f6cb305cdffbca48f85bc0e0fb349ff3024d93
hjjo <hjjo0927@gmail.com>
2024-12-30 16:15:12 +0900

    fix : 시큐리티 설정으로 인한 showtime 경로 분리

--- src/main/java/com/example/letmovie/domain/movie/controller/ShowtimeController.java
+++ src/main/java/com/example/letmovie/domain/movie/controller/ShowtimeController.java
@@ -6,21 +6,18 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/movie")
 public class ShowtimeController {
 
     private final ShowtimeServiceImpl showtimeService;
 
-    @GetMapping("/showtime")
+    @GetMapping({"/movie/showtime", "/private/movie/showtime"})
     public String getShowtime(Model model) {
         List<ShowtimeDTO> showtime = showtimeService.getAllShowtime();
 
