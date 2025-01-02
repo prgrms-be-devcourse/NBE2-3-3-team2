4fc4942d6da3ba9e4bd389c546dde7c8b20260f1
JinYoung Ko <jim00011128@gmail.com>
2025-01-02 12:16:17 +0900

    refactor : 좌석 alert 내용 변경 #SCRUM-78

--- src/test/java/com/example/letmovie/domain/reservation/controller/ReservationControllerTest.java
+++ src/test/java/com/example/letmovie/domain/reservation/controller/ReservationControllerTest.java
@@ -1,4 +1,45 @@
 package com.example.letmovie.domain.reservation.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@SpringBootTest
 public class ReservationControllerTest {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    @BeforeEach
+    void clean() {
+    }
+
+    @Test
+    @DisplayName("영화조회")
+    void test1() throws Exception {
+        //given
+
+        //expected
+        mockMvc.perform(post("")
+                        .content(objectMapper.writeValueAsString("test"))
+                        .contentType(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print());
+
+    }
 }
