68951e274e93894c89fd72b91752189e4f5bd895
suho <yousuho1232@naver.com>
2025-01-22 12:46:03 +0900

    refactor : 영화 검색에 대한 인덱스 추가

--- src/main/java/com/example/letmovie/domain/movie/entity/Movie.java
+++ src/main/java/com/example/letmovie/domain/movie/entity/Movie.java
@@ -11,40 +11,59 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(
+        name = "movie",
+        indexes = {
+                @Index(name = "idx_movie_name", columnList = "movieName"), // movieName 컬럼에 대한 인덱스
+        }
+)
 public class Movie {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "movie_id")
     private Long id;
+
     @Column(nullable = false)
     private String movieName;
+
     @Column(nullable = false)
     private String movieCode;
+
     @Column(nullable = false)
     private String directorName;
+
     @Column(nullable = false)
     private String rating;
+
     @Column(nullable = false)
     private String runtime;
+
     @Column(nullable = false)
     private String openDate;
+
     @Column(nullable = false)
     private String genreName;
+
     @Column(nullable = false)
     private String companys;
+
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Status status;
+
     @Column(nullable = false)
     private String posterImageUrl;
+
     @Column(nullable = false)
     private String stillImageUrl;
+
     @Column(nullable = false)
     private String plot;
+
     @Column(nullable = false)
     private String salesAcc;
+
     @Column(nullable = false)
     private String audiAcc;
-
 }
