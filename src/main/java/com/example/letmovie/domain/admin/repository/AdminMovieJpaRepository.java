878c95e97d2c7ec5d340b7f6e02b63c54d50998f
JinYoung Ko <jim00011128@gmail.com>
2025-01-31 18:16:09 +0900

    refactor :  admin 영화 검색 쿼리 로직 변경

--- src/main/java/com/example/letmovie/domain/admin/repository/AdminMovieJpaRepository.java
+++ src/main/java/com/example/letmovie/domain/admin/repository/AdminMovieJpaRepository.java
@@ -13,7 +13,7 @@ public interface AdminMovieJpaRepository extends JpaRepository<Movie, Long> {
     @Query(value = "select m from Movie m")
     List<Movie> findAllMovies();
 
-    @Query(value = "select m from Movie m where m.movieName like %:movieName%")
+    @Query(value = "SELECT m FROM Movie m WHERE m.movieName LIKE CONCAT(:movieName, '%')")
     List<Movie> findMovieByName(@Param("movieName") String movieName);
 
     /*@Query(value = "select m from Movie m where m.movieName like %:movieName%")
